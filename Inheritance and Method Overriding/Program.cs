0103d260db4f63156b6b74f51f3ea61342f6e561
11293859-MUBARAK WUMPINI ABDUL-RASHID <mwabdul-rashid@st.ug.edu.gh>
2025-08-08 17:52:24 +0000

    iniheritance and method overloading

---
+++ Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Inheritance_and_Method_Overriding
+{
+class Animal
+{
+    // Virtual method that can be overridden by derived classes
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    // Override the MakeSound method
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    // Override the MakeSound method
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create instances of each class
+        Animal genericAnimal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        // Call MakeSound on each
+        genericAnimal.MakeSound(); // Output: Some generic sound
+        dog.MakeSound();           // Output: Bark
+        cat.MakeSound();           // Output: Meow
+
+        Console.ReadLine();
+    }
+}
+}
