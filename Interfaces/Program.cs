ff4598a9f440f62c21e0562e25b885940792afc2
11293859-MUBARAK WUMPINI ABDUL-RASHID <mwabdul-rashid@st.ug.edu.gh>
2025-08-08 18:15:18 +0000

    intefaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Interfaces
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Class Car implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Class Bicycle implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            // Call Move() on each
+            car.Move();      // Output: Car is moving
+            bicycle.Move();  // Output: Bicycle is moving
+
+            Console.ReadLine();
+        }
+    }
+}
